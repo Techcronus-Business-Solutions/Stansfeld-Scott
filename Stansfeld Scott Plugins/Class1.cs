ba17897eb9ceb7c6bee6e4fec2714a3fb3e6dc20
Shivang Simaria <shivang.simaria@techcronus.com>
2026-09-24 13:15:13 +0530

    Initial Setup

---
+++ Stansfeld Scott Plugins/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stansfeld_Scott_Plugins
+{
+    public class Class1
+    {
+    }
+}
